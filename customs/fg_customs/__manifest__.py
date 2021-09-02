678131a165e0aa3f76b0d45021bbb226877ef6fd
ABOITIZ\CRacaza <cynthia.racaza@aboitiz.com>
2021-09-02 17:16:04 +0800

    add custom folder for custom addons

---
+++ customs/fg_customs/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+   'name': 'FG Custom Addons',
+    'version': '1.0',
+    'category': 'FG/FG',
+    'description': """
+    Pilmico custom addons
+        """,
+    'depends': ['product','website'],
+    'data': [
+    ],
+    'author': "1FG",
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False
+}
---
+++ customs/fg_customs/models/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+# sample import below
+# from . import fg_custom_product
+
