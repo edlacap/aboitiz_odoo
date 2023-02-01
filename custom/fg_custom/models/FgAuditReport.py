40eb6eb6761196a2410d78ef34d3173860e90b55
Jonan <jonan.toquia@aboitiz.com>
2023-02-01 17:34:15 +0800

    Audit Log Report

--- custom/fg_custom/models/FgAuditReport.py
+++ custom/fg_custom/models/FgAuditReport.py
@@ -5,75 +5,39 @@ from datetime import datetime
 from dateutil.relativedelta import relativedelta
 from odoo.exceptions import UserError
 
-# class AuditLog(models.AbstractModel):
-#     _name = 'audit.log'
-
 class FgAuditReport(models.Model):
     _name = 'report.fg_custom.report_fg_audit_report'
     _inherit = 'audit.log'
     _description = 'Audit Log Report'
 
     def action_print(self, data):
+        headers = 'Date,Resource Name,User,Last Updated On, Field, Old Value, New Value'
+        run_report_date = datetime.today().strftime('%Y-%m-%d')
+        company = self.env.company
+        report_type = 'Audit Log Report'
+        audit_report = []
+
         auditRec = self.env['audit.log'].search([('create_date', '>=', data.get('date_start'))])
 
-        # headers = 'SI#,Order Date,Total Items,Total Amount,Vat Exempt, Vat Zero-Rated, Vatable,VAT,PWD/SC Discount,PWD/SC Discount Amount,Other Discount,Total Amount Paid'  #will change depends on report type, by default, daily sales report
-        headers = 'Name'
-        audit_report = []
-        run_report_date = datetime.today().strftime('%Y-%m-%d')
-        # auditLogs = name
-        company = self.env.company
-
-        # for audit in auditRec:
-
-
-        # if data.get('report_type') == 'audit_report':
-        # auditLogs = self.env['audit.log'].search([('create_date', '>=', data.get('date_start')), ('create_date', '<=', data.get('date_stop'))])
-
-            # report_type = 'Audit Log Report'
-
-        # if data.get('report_type') == 'sales_report' or data.get('report_type') == 'pwd_sc_discount':
-        #     orders = self.env['pos.order'].search([('pos_si_trans_reference', '!=', False), ('date_order', '>=', data.get('date_start')),('date_order', '<=', data.get('date_stop'))])
-        #     report_type = 'Sales Report'
-        #     if data.get('report_type') == 'pwd_sc_discount':
-        #         report_type = 'PWD and SC Discount Sales Report'
-        # elif data.get('report_type') == 'refund_report':
-        #     orders = self.env['pos.order'].search([('pos_refund_si_reference', '!=', False), ('date_order', '>=', data.get('date_start')), ('date_order', '<=', data.get('date_stop'))])
-        #     report_type = 'Refund Report'
-        #     headers = 'Refund SI#,Reference SI#,Refund Date,Total Items,Total Amount,Vat Exempt, Vat Zero-Rated, Vatable,VAT,PWD/SC Discount,PWD/SC Discount Amount,Other Discount,Total Amount Paid'  # will change depends on report type, by default, daily sales report
-
         for logs in auditRec:
-            #get computed values in order lines
-            # line_computed_values = logs._get_lines_computed_values(order)
-            # name = logs.name;
-            audit_report.append({'name': auditRec.name,
-                                 'date': auditRec.create_date,
-                                 'resource_name': auditRec.model,
-                                 'last_update_on': auditRec.write_date,
-                                 'data': auditRec.data})
+            updated_details = []
+            content = logs._get_content()
 
-            # audit_report.append({'name': name})
+            for det in content:
+                updated_details.append({'field': det[0],
+                                        'oldValue': det[1],
+                                        'newValue': det[2]
+                                        })
+            # print(updated_details)
 
-            # refNumber = None #refund/si reference
-            # siReferenceNumber = None #si reference or refunded order
-            # if report_type == 'Sales Report' or report_type == 'PWD and SC Discount Sales Report':
-            #     refNumber = order.pos_si_trans_reference
-            # elif report_type == 'Refund Report':
-            #     refNumber = order.pos_refund_si_reference
-            #     siReferenceNumber = order.pos_refunded_id.pos_si_trans_reference
-
-
-            # if data.get('report_type') == 'sales_report' or data.get('report_type') == 'refund_report'or (data.get('report_type') == 'pwd_sc_discount' and line_computed_values.get('government_total_discount')!=0):
-            #     order_report.append({'refNum': refNumber, 'reference_si': siReferenceNumber, 'date_order' : order.date_order.strftime('%Y-%m-%d %H:%M:%S'),'vat': order.amount_tax, 'amount_paid': order.amount_paid,
-            #                          'total_discount': line_computed_values.get('total_discount'), 'total_product_v': line_computed_values.get('total_product_v'),
-            #                          'total_vat': line_computed_values.get('total_vat'),'total_product_e': line_computed_values.get('total_product_e'),
-            #                          'total_qty': line_computed_values.get('total_qty'),'government_discount_name': line_computed_values.get('government_discount_name'),
-            #                          'government_total_discount': line_computed_values.get('government_total_discount'), 'total_amount': line_computed_values.get('total_amount'),
-            #                          'total_product_z': line_computed_values.get('total_product_z')
-            #                          })
-
-
+            audit_report.append({'date': fields.Datetime.to_string(logs.create_date),
+                                 'name': logs.name,
+                                 'user': logs.user_id.name,
+                                 'last_update_on': fields.Datetime.to_string(logs.write_date),
+                                 'data': updated_details
+                                 })
         returndata = {
-            # 'report_type': report_type,
+            'report_type': report_type,
             'run_report_date': run_report_date,
             'generated_by': self.env.user.name,
             'name': 'The Good Meat -' + company.name,
